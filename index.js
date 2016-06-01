b139e095466dd9176e2d75158c29f988adb13889
lrlna <shestak.irina@gmail.com>
2016-05-31 17:31:36 -0700

    set and assign neighbours to each cell -- each cell has knowledge of it's neighbours

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ var fs = require("fs")
 function Automata () {
   var self = {}
 
-  self.lattice = {}
+  self.lattice = []
 
   // create automata lattice based on size provided
-  self.set = function(size) {
+  self.set = function (size) {
     if (!isNumber(size)) return false
 
-    size.forEach(function(number) {
-      self.lattice.state = getRandomState()
-      self.
+    // start at a random state
+    size.forEach(function (number) {
+      var cell = {}
+      cell.state = getRandomState()
+      self.lattice.push(cell.state)
     })
+
+    // assign all the neighbours
+    self.setNeighbours()
 
     return self.automata
   }
 
   // figure out neighbourhoods
-  self.neighbours = function () {
+  self.getNeighbours = function () {
 
+    return self.getNeighbours
+  }
+
+  self.setNeighbours = function () {
+
+    return self.setNeighbours
+  }
+
+  self.rules = function () {
+
+    return self.rules
   }
 
   function isNumber (x) {
     var number = (typeof x === 'number') ? true : false
-    return number 
+    return number
   }
 
   function getRandomState () {
@@ -33,3 +49,50 @@ function Automata () {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 }
+
+/* possible structure:
+ * [
+ *   // lattice
+ *   [
+ *     // cell
+ *     {
+ *       state: true,
+ *       leftNeighbour: true,
+ *       rightNeighbour: false
+ *     },
+ *     // cell
+ *     {
+ *       state: true,
+ *       leftNeighbour: true,
+ *       rightNeighbour: false
+ *     },
+ *     // cell
+ *     {
+ *       state: true,
+ *       leftNeighbour: true,
+ *       rightNeighbour: false
+ *     }
+ *   ],
+ *   // lattice
+ *   [
+ *     // cell
+ *     {
+ *       state: true,
+ *       leftNeighbour: true,
+ *       rightNeighbour: false
+ *     },
+ *     // cell
+ *     {
+ *       state: true,
+ *       leftNeighbour: true,
+ *       rightNeighbour: false
+ *     },
+ *     // cell
+ *     {
+ *       state: true,
+ *       leftNeighbour: true,
+ *       rightNeighbour: false
+ *     }
+ *   ]
+ * ]
+ */
