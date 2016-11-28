ec8561b61e51f3159386fa0f600c02729ca4d782
lrlna <shestak.irina@gmail.com>
2016-11-28 06:06:51 +0000

    ok, let's set up a neighbourhood matrix with rule and neighbourhoods

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ const Automata = () => {
 
   const neighbourhoods = [ '000', '001', '010', '011', '100', '101', '110', '111' ]
 
+  // need some automata accessible variables
   self.automata = []
-
   self.rule
+  self.neighbourhoodMatrix
 
   // create automata lattice based on size (number of cells) provided
   // run over a set number of a given life
   self.set = (size, rule, life) => {
-    // assign a rule binary
-    self.rule = self.getRules(rule)
-
     // possibly check whether there is already a cell in automata
     var lattice = []
-
+    // assign a rule binary
+    self.rule = self.getRules(rule)
+    self.neighbourhoodMatrix = self.setRules()
     lattice = self.setState(lattice, size)
     lattice = self.setNeighbours(lattice)
     // modify the lattice to have the whole neighbourhood
     lattice = self.getNeighbours(lattice)
     // automata stores each lattice procedurely
     self.automata.push(lattice)
+    self.runRules(rule, self.automata)
 
     return self.set
   }
 
-  // get a random state and create all cells 
+  // get a random state and create all cells
   self.setState = (lattice, size) => {
     for (var num = 0; num < size; num++) {
       var cell = {}
@@ -39,8 +40,8 @@ const Automata = () => {
 
   // figure out who your neighbour are
   self.getNeighbours = (lattice) => {
-    return lattice.map( (cell) => {
-      var states = Object.keys(cell).map( (key) =>{
+    return lattice.map(cell => {
+      var states = Object.keys(cell).map(key =>{
         return cell[key]
       })
       // join states into a single neighbourhood
@@ -51,7 +52,7 @@ const Automata = () => {
 
   // set neighbourhoods for the current initial lattice
   self.setNeighbours = (lattice) => {
-    return lattice.map(function (cell, index, array) {
+    return lattice.map( (cell, index, array) => {
       var lastEl = lattice.length-1
 
       // if first cell, it's left neighbour is the last cell
@@ -85,11 +86,27 @@ const Automata = () => {
     return ("000000000" + parseInt(num, 10).toString(2)).substr(-8)
   }
 
-  self.runRules = (lattice, rules) => {
-    // check if we have an automata to run rules on
-    // otherwise throw an error
-    if (!self.automata.length) return false
-    var currentYear = self.automata.slice(-1)[0]
+  self.setRules = (rule) => {
+    if (!rule) rule = self.rule
+    rule = [...rule]
+    // i am keeping 'hood', no one argue with me
+    return neighbourhoods.map( (hood, index) => {
+      return {
+        hood: hood,
+        rule: rule[index]
+      }
+    })
+  }
+
+  self.runRules = (rules, automata) => {
+    // can also pass an automata externally
+    var automaton = self.automata || automata
+    // get the last array
+    var lastLife = automaton.slice(-1)[0]
+    var newState = lastLife.map(cell => {
+    })
+
+    return automaton.push(newState)
   }
 
   self.nextGeneration = () => {
